9755f839f5a099754a60c54835ee869a35e5ebda
Juliano Afonso <julianoafonsso1@gmail.com>
2017-06-16 13:13:33 -0300

    Update loginControler.php

--- loginControler.php
+++ loginControler.php
@@ -2,34 +2,41 @@
 	
 	include 'conecta.php';
 
+	if (!$db) {
+	    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+	    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+	    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    	exit;
+	}
+
 	if(empty($_POST["login"]) || empty($_POST["senha"])) {
 
-		echo"<script language='javascript' type='text/javascript'>alert('Preencha ambos os campos');window.location.href='login.php';</script>";
+		echo "empty";
 
 	} else {
 
 		$login = $_POST['login'];	
 	  	$senha = $_POST['senha'];
 	  
-	  	$consulta =	"SELECT func_id, func_nome, func_login, func_senha, tipo_tipo_id
-	               	 FROM funcionario
-	               	 WHERE func_login = '$login' and func_senha = '$senha' ";  
+	  	$consulta 	=	"SELECT func_id, func_nome, func_login, func_senha, tipo_tipo_id
+	               	 	FROM funcionario
+	               	 	WHERE func_login = '$login' and func_senha = '$senha' ";  
 
-	    $resultado = mysqli_query($db, $consulta); 
-	    $executa = mysqli_fetch_array($resultado);
+	    $resultado 	= mysqli_query($db, $consulta); 
+	    $executa 	= mysqli_fetch_array($resultado);
 
 
 	 	if( $executa == NULL) {
 
-	    	echo"<script language='javascript' type='text/javascript'>alert('Login e/ou senha incorretos');window.location.href='login.php';</script>";
-	        die();
+			echo "false";
 
 	   	} else {
 
-	    	$_SESSION['login'] = $login;
-	    	header("location: paginademesas.php");
+			echo "true";
 
 	    }
 
 	}
+
+
 ?>
